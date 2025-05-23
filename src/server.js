a8e6e1111a5314d8932ec6a91ad82fbcba96b73e
WendsonMagalhaes <wendson.silva@ccc.ufcg.edu.br>
2025-05-22 21:18:49 -0300

    Ativando CORS para todas as origens

--- src/server.js
+++ src/server.js
@@ -1,15 +1,23 @@
 // src/server.js
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const usuarioRoutes = require('./routes/usuarioRoutes');
 require('dotenv').config();
 
 const app = express();
 
+app.use(cors());
+
+// Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
 
+// Rotas
 app.use('/usuarios', usuarioRoutes);
 
+// Iniciar servidor
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Servidor rodando em http://localhost:${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
